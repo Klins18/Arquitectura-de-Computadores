39451073290846508e129cbf91822b8d5e0c5a4a
Klins18 <102160876+Klins18@users.noreply.github.com>
2025-11-04 23:33:00 -0500

    Update macro.asm

--- MIPS/macro.asm
+++ MIPS/macro.asm
@@ -1,23 +1,23 @@
-# Macro para branch if less than
-.macro blt (%rs, %rt, %label)
-    slt $at, %rs, %rt
-    bne $at, $zero, %label
+# Macro para branch if less than 
+.macro blt_macro ($rs, $rt, $label)
+    slt $at, $rs, $rt
+    bne $at, $zero, $label
 .end_macro
 
 # Macro para branch if greater than  
-.macro bgt (%rs, %rt, %label)
-    slt $at, %rt, %rs
-    bne $at, $zero, %label
+.macro bgt_macro ($rs, $rt, $label)
+    slt $at, $rt, $rs
+    bne $at, $zero, $label
 .end_macro
 
 # Macro para branch if less or equal
-.macro ble (%rs, %rt, %label)
-    slt $at, %rt, %rs
-    beq $at, $zero, %label
+.macro ble_macro ($rs, $rt, $label)
+    slt $at, $rt, $rs
+    beq $at, $zero, $label
 .end_macro
 
 # Macro para branch if greater or equal
-.macro bge (%rs, %rt, %label)
-    slt $at, %rs, %rt
-    beq $at, $zero, %label
+.macro bge_macro ($rs, $rt, $label)
+    slt $at, $rs, $rt
+    beq $at, $zero, $label
 .end_macro
